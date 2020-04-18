bb35bf7c89e1ea5365e184d3d8ee8f02abd77b94
bplovell <54449704+bplovell@users.noreply.github.com>
2020-04-18 13:23:19 -0500

    adding root emails for registrations

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, pre_save
 from .models import AdtaaUser, Profile
 from django.dispatch import receiver
-from django.core.mail import send_mail, send_mass_mail
+from django.core.mail import send_mail
 from django.conf import settings
 
 superusers_emails = AdtaaUser.objects.filter(is_superuser=True).values_list('email', flat='true')
