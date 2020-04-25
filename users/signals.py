20c3c3c3b5cc3d4b2bb6d7475cf58b22a44c17d2
bplovell <54449704+bplovell@users.noreply.github.com>
2020-04-25 14:20:12 -0500

    correcting import

--- users/signals.py
+++ users/signals.py
@@ -3,10 +3,10 @@ from .models import AdtaaUser, Profile
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
-from django.contrib.sites.models import Site
+from django.contrib.sites.shortcuts import get_current_site
 
 superusers_emails = AdtaaUser.objects.filter(is_superuser=True).values_list('email', flat='true')
-site = Site.objects.get_current()
+current_site = Site.objects.get_current()
 
 @receiver(post_save, sender=AdtaaUser)
 def create_profile(sender, instance, created, **kwargs):
@@ -34,7 +34,7 @@ def send_email_to_root(sender, instance, created, **kwargs):
 def active(sender, instance, **kwargs):
     if instance.is_active and AdtaaUser.objects.filter(pk=instance.pk, is_active=False).exists():
         subject = 'Active account'
-        message = '{}, Your ADTAA account is now active.  You may login here: %s/login '.format(instance.username) % site.name
+        message = '{}, Your ADTAA account is now active.  You may login here: %s/login '.format(instance.username) % current_site.name
         # from_email = settings.EMAIL_HOST_USER
         from_email = 'coffeeloversusa2020@gmail.com'
         to_email = [instance.email]
